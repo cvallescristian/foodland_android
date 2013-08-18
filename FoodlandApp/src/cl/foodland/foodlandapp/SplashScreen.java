747a72717cd32d12d1b65dbd21d0f2e345aa2cdf
VitoVoti <victorafro@gmail.com>
2013-08-17 22:22:40 -0400

    proyecto creado + icon + splash
    hice el proyecto, va de 2.2 a 4.3 asi que actualicen su adt si es
    necesario. Ah, y le puse un icono y una pantalla splash simple (por
    mientras, con tiempo de 3000ms, de ahi podemos rellenar con lo del
    query). Aun tengo que ver como quitar la barrita de arriba.

---
+++ FoodlandApp/src/cl/foodland/foodlandapp/SplashScreen.java
@@ -0,0 +1,32 @@
+package cl.foodland.foodlandapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+	// tiempo en que se ve la pantalla de splash, en milisegundos
+	private static int TIEMPO_ESPERA = 3000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		//handler está con delay, luego abre foodland
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				//cuando acabe el timer, se abre la app de foodland
+				Intent i = new Intent(SplashScreen.this, MainActivity.class);
+				startActivity(i);
+
+				// cierra la actividad
+				finish();
+			}
+		}, TIEMPO_ESPERA);
+	}
+
+}
